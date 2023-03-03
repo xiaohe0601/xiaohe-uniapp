cc4d0f4c263cdbfb93510bc83de3f7aea7c82b00
MyHdg0601 <HeDianGeng0601@outlook.com>
2023-03-03 14:23:12 +0800

    新增 递归遍历树形结构数组工具方法(ArrayUtils.recursiveTraversal)

--- utils/array.js
+++ utils/array.js
@@ -2,16 +2,16 @@ class ArrayUtils {
   /**
    * @typedef Flat2TreeOptions  扁平化数组转树形解构-配置项
    *
-   * @property {string} [key="id"]          节点唯一标识属性名
-   * @property {string} [parentKey="pid"]   父节点唯一标识属性名
-   * @property {(any) => any} [processor]   节点数据处理器
+   * @property {string} [key="id"]                      节点唯一标识属性名
+   * @property {string} [parentKey="pid"]               父节点唯一标识属性名
+   * @property {(item: any) => item: any} [processor]   节点数据处理器
    */
   /**
-   * 扁平数组转树形结构
+   * 扁平数组转树形结构数组
    *
    * @param {any[]} array                   扁平数组
    * @param {Flat2TreeOptions} [options]    配置项
-   * @return {any[]}  树形结构
+   * @return {any[]}  树形结构数组
    */
   static flat2tree(array, options = {}) {
     const { key, parentKey, processor } = Object.assign({}, {
@@ -19,6 +19,10 @@ class ArrayUtils {
       parentKey: "pid",
       processor: (item) => (item)
     }, options);
+
+    if (array == null) {
+      return [];
+    }
 
     const map = array.reduce((previous, item) => {
       previous[item[key]] = item;
@@ -46,15 +50,15 @@ class ArrayUtils {
   }
 
   /**
-   * @typedef Tree2FlatOptions  树形结构转扁平数组-配置项
+   * @typedef Tree2FlatOptions  树形结构数组转扁平数组-配置项
    *
-   * @property {string} [childrenKey="children"]    子节点集合属性名
-   * @property {(any) => any} [processor]           节点数据处理器
+   * @property {string} [childrenKey="children"]        子节点集合属性名
+   * @property {(item: any) => item: any} [processor]   节点数据处理器
    */
   /**
-   * 树形结构转扁平数组
+   * 树形结构数组转扁平数组
    *
-   * @param {any[]} array                   树形结构
+   * @param {any[]} array                   树形结构数组
    * @param {Tree2FlatOptions} [options]    配置项
    * @return {any[]}  扁平数组
    */
@@ -63,6 +67,10 @@ class ArrayUtils {
       childrenKey: "children",
       processor: (item) => (item)
     }, options);
+
+    if (array == null) {
+      return [];
+    }
 
     return array.reduce((previous, { [childrenKey]: children, ...others }) => {
       if (children && children.length > 0) {
@@ -73,6 +81,38 @@ class ArrayUtils {
 
       return previous;
     }, []);
+  }
+
+  /**
+   * @typedef RecursiveTraversalOptions  递归遍历树形结构数组-配置项
+   *
+   * @property {string} [childrenKey="children"]                子节点集合属性名
+   * @property {any} [parent]                                   父节点数据
+   * @property {(item: any, parent: any) => void} [processor]   节点数据处理器
+   */
+  /**
+   * 递归遍历树形结构数组
+   *
+   * @param {any[]} array                         树形结构数组
+   * @param {RecursiveTraversalOptions} options   配置项
+   */
+  static recursiveTraversal(array, options = {}) {
+    const { childrenKey, processor } = Object.assign({}, {
+      childrenKey: "children"
+    }, options);
+
+    if (array == null) {
+      return;
+    }
+
+    for (const item of array) {
+      const children = item[childrenKey];
+      if (children && children.length > 0) {
+        ArrayUtils.recursiveTraversal(children, Object.assign({}, options, { parent: item }))
+      }
+
+      processor && processor(item, options.parent);
+    }
   }
 }
 
