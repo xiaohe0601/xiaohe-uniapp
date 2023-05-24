6c9dd2a2dfe300bce27d871abfa81539837f04ed
小何同学 <HeDianGeng0601@outlook.com>
2023-05-24 16:07:43 +0800

    新增 AppPicker的VueDoc

--- components/AppPicker.vue
+++ components/AppPicker.vue
@@ -24,6 +24,42 @@
 <script>
 import _ from "lodash";
 
+/**
+ * AppPicker 选择器
+ *
+ * @author        小何同学 (xiaohe0601)
+ * @description   本组件用于单列或多列选项选择。
+ *
+ * @property {Boolean}          show                  是否展示选择器 <支持.sync>
+ * @property {Boolean}          value                 当前绑定值 <支持v-model>
+ * @property {Boolean}          showToolbar           是否显示顶部的操作栏
+ * @property {String}           title                 顶部标题
+ * @property {Array}            columns               每一列的数据列表
+ * @property {Boolean}          singleColumn          是否为单列模式 (若为true, 则columns只需要传递1维数组, 内部会自动构造为u-picker所需的二维数组)
+ * @property {Boolean}          loading               是否显示加载中状态
+ * @property {String}           confirmText           确认按钮的文字
+ * @property {String}           cancelText            取消按钮的文字
+ * @property {String|Number}    itemHeight            各列中单个选项的高度
+ * @property {Number}           visibleItemCount      每列中可见选项的数量
+ * @property {String}           uniqueKey             唯一标识键名 (对应的属性值应即为v-model的值, 亦用于定位当前选中位置)
+ * @property {String}           keyName               展示文本属性键名
+ * @property {Boolean}          asyncClose            是否异步关闭
+ * @property {Boolean}          closeOnConfirm        点击确定按钮时是否关闭选择器
+ * @property {Boolean}          closeOnClickOverlay   点击遮罩是否关闭选择器
+ * @property {Array}            defaultIndex          各列的默认索引
+ * @property {Boolean}          reserveIndexs         是否保留上次滚动位置 (若为false则会在每次展示时回到当前绑定值所在位置)
+ * @property {Boolean}          immediateChange       是否在手指松开时立即触发change事件
+ *
+ * @event {Function} update:show    是否展示选择器变化 [value: 是否展示选择器]
+ * @event {Function} input          点击确定 [value: 选中行唯一标识值(singleColumn为false时, 返回数组表示各列选中行的唯一标识值)]
+ * @event {Function} confirm        点击确定 [{indexs: 各列选中项索引, value: 各列选中项的值, values: 各列列表数据}]
+ * @event {Function} change         当选择值变化时触发 [{indexs: 各列选中项索引, value: 各列选中项的值, values: 各列列表数据, index: 当前变化列选中项索引, columnIndex: 当前变化列索引}]
+ * @event {Function} cancel         点击取消
+ * @event {Function} close          选择器关闭
+ *
+ * @example
+ * <app-picker v-model="current" :show.sync="show" :columns="records"></app-picker>
+ */
 export default {
   name: "AppPicker",
   options: {
